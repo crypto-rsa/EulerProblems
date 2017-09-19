ef6b8a1f56d4b4cd560088af88fa8f703b774892
crypto_rsa <pkotrc@gmail.com>
2017-09-19 11:43:21 +0200

    Add the IProblem interface

---
+++ Solutions/IProblem.cs
@@ -0,0 +1,20 @@
+﻿namespace EulerProblems.Solutions
+{
+    /// <summary>
+    /// Represents a single problem
+    /// </summary>
+    /// <typeparam name="TArgs">The type of the problem arguments</typeparam>
+    public interface IProblem<in TArgs>
+    {
+        #region Methods
+
+        /// <summary>
+        /// Solves the problem
+        /// </summary>
+        /// <param name="arguments">The problem arguments</param>
+        /// <returns>The string representation of the problem solution</returns>
+        string Solve( TArgs arguments );
+
+        #endregion
+    }
+}
