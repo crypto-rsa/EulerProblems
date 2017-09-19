e565496efb27b6bbb36b20bea175ff860bb829cf
crypto_rsa <pkotrc@gmail.com>
2017-09-19 11:44:34 +0200

    Add class SingleLimitProblemArgs

---
+++ Solutions/SingleLimitProblemArgs.cs
@@ -0,0 +1,30 @@
+﻿namespace EulerProblems.Solutions
+{
+    /// <summary>
+    /// Represents problem arguments with a single limit of type <see cref="long"/>
+    /// </summary>
+    public class SingleLimitProblemArgs
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructs an instance of the <see cref="SingleLimitProblemArgs"/> with the given limit
+        /// </summary>
+        /// <param name="limit">The limit</param>
+        public SingleLimitProblemArgs( long limit )
+        {
+            Limit = limit;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or set the limit
+        /// </summary>
+        public long Limit { get; private set; }
+
+        #endregion
+    }
+}
