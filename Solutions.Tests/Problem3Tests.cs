561bf87cdf828d6e64b2098f6f77eca2939e6067
crypto_rsa <pkotrc@gmail.com>
2017-09-20 13:47:32 +0200

    Add problem #3

---
+++ Solutions.Tests/Problem3Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace EulerProblems.Solutions.Tests
+{
+    [TestFixture]
+    internal class Problem3Tests
+    {
+        #region Methods
+
+        [Test]
+        [TestCase(2, 2)]
+        [TestCase(8, 2)]
+        [TestCase(21, 7)]
+        [TestCase(97, 97)]
+        [TestCase(1000, 5)]
+        public void SolutionShouldMatch( long number, long expectedResult )
+        {
+            var problem = new Problem3();
+
+            var result = problem.Solve( new SingleLimitProblemArgs( number ) );
+
+            Assert.AreEqual( result, expectedResult.ToString() );
+        }
+
+        #endregion
+    }
+}
---
+++ Solutions/Problem3.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Tools;
+
+namespace EulerProblems.Solutions
+{
+    /// <summary>
+    /// Problem #3 (https://projecteuler.net/problem=3)
+    /// </summary>
+    public class Problem3 : IProblem<SingleLimitProblemArgs>
+    {
+        #region Methods
+
+        public string Solve( SingleLimitProblemArgs arguments )
+        {
+            return new Primes().Factor( arguments.Limit ).Last().prime.ToString();
+        }
+
+        #endregion
+    }
+}
