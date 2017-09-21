eef7915888a75aa373257151bc0e005fc9dfb4f5
crypto_rsa <pkotrc@gmail.com>
2017-09-21 15:43:37 +0200

    Add class Palindromes

---
+++ Tools.Tests/PalindromesTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Tools.Tests
+{
+    [TestFixture]
+    internal class PalindromesTests
+    {
+        #region Methods
+
+        [Test]
+        [TestCase( 1, 10, true )]
+        [TestCase( 9, 10, true )]
+        [TestCase( 2221, 10, false )]
+        [TestCase( 0b11011, 2, true )]
+        [TestCase( 0xABBA, 16, true )]
+        [TestCase( 15, 10, false )]
+        [TestCase( 0b101111, 2, false )]
+        [TestCase( 0xDEADBEEF, 16, false )]
+        public void IsPalindromic_ShouldBeTrueForPalindromesOnly( long number, byte @base, bool expectedValue )
+        {
+            bool value = Palindromes.IsPalindromic( number, @base );
+
+            Assert.AreEqual( value, expectedValue );
+        }
+
+        #endregion
+    }
+}
---
+++ Tools/Palindromes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tools
+{
+    /// <summary>
+    /// Contains methods for working with palindromes
+    /// </summary>
+    public static class Palindromes
+    {
+        #region Methods
+
+        public static bool IsPalindromic( long number, byte @base )
+        {
+            if( @base < 2 )
+                throw new ArgumentOutOfRangeException( nameof( @base ), "The base must be at least 2!" );
+
+            var digits = new List<int>( 64 );
+
+            long n = number;
+            while( n > 0 )
+            {
+                int d = (int) (n % @base);
+                n /= @base;
+
+                digits.Add( d );
+            }
+
+            return Enumerable.Range( 0, (digits.Count + 1) / 2 ).All( i => digits[i] == digits[digits.Count - i - 1] );
+        }
+
+        #endregion
+    }
+}
