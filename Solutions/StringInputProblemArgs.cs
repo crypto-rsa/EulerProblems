0152d5829eaba089a17c1dfc05fc41f25ad00610
crypto_rsa <pkotrc@gmail.com>
2017-09-22 09:24:58 +0200

    Add class StringInputProblemArgs

---
+++ Solutions/StringInputProblemArgs.cs
@@ -0,0 +1,30 @@
+﻿namespace EulerProblems.Solutions
+{
+    /// <summary>
+    /// Represents problem arguments with a single string input
+    /// </summary>
+    public class StringInputProblemArgs
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructs an instance of the <see cref="StringInputProblemArgs"/> class with the given string
+        /// </summary>
+        /// <param name="input">The input string</param>
+        public StringInputProblemArgs( string input )
+        {
+            Input = input;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the input string
+        /// </summary>
+        public string Input { get; private set; }
+
+        #endregion
+    }
+}
