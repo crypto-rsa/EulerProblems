fe119b1ed81843f773f6b8b4ab952e63d8db8691
crypto_rsa <pkotrc@gmail.com>
2017-12-02 14:14:42 +0100

    Add class Factorization

---
+++ Tools.Tests/FactorizationTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tools.Tests
+{
+    [TestFixture]
+    internal class FactorizationTests
+    {
+        [TestCase( 0 )]
+        [TestCase( -10 )]
+        public void Of_ShouldThrowArgumentOutOfRangeException_WhenLessThanOne( long number )
+        {
+            Assert.That( () => Factorization.Of( number ), Throws.Exception.TypeOf<ArgumentOutOfRangeException>() );
+        }
+
+        [Test]
+        public void Of_ShouldReturnEmpty_ForOne()
+        {
+            Assert.That( Factorization.Of( 1 ), Is.EqualTo( Factorization.Empty ) );
+        }
+
+        [TestCaseSource( nameof( GetFactorizationTestCases ) )]
+        public void Of_ShouldReturnCorrectValue( long number, IEnumerable<(long, int)> expected )
+        {
+            Assert.That( Factorization.Of( number ), Is.EqualTo( new Factorization( expected ) ) );
+        }
+
+        private static IEnumerable<object[]> GetFactorizationTestCases()
+        {
+            yield return new object[] { 2, new(long, int)[] { (2, 1) } };
+            yield return new object[] { 37, new(long, int)[] { (37, 1) } };
+            yield return new object[] { 48, new(long, int)[] { (2, 4), (3, 1) } };
+            yield return new object[] { 900, new(long, int)[] { (2, 2), (3, 2), (5, 2) } };
+            yield return new object[] { 1_348, new(long, int)[] { (2, 2), (337, 1) } };
+            yield return new object[] { 8_778, new(long, int)[] { (2, 1), (3, 1), (7, 1), (11, 1), (19, 1) } };
+        }
+
+        [Test]
+        public void OfFactorial_ShouldReturnEmpty_ForOne()
+        {
+            Assert.That( Factorization.OfFactorial( 1 ), Is.EqualTo( Factorization.Empty ) );
+        }
+
+        [TestCaseSource( nameof( GetFactorialFactorizationTestCases ) )]
+        public void OfFactorial_ShouldReturnCorrectValue( int number, IEnumerable<(long, int)> expected )
+        {
+            Assert.That( Factorization.OfFactorial( number ), Is.EqualTo( new Factorization( expected ) ) );
+        }
+
+        private static IEnumerable<object[]> GetFactorialFactorizationTestCases()
+        {
+            yield return new object[] { 0, new(long, int)[] { } };
+            yield return new object[] { 2, new(long, int)[] { (2, 1) } };
+            yield return new object[] { 3, new(long, int)[] { (2, 1), (3, 1) } };
+            yield return new object[] { 5, new(long, int)[] { (2, 3), (3, 1), (5, 1) } };
+            yield return new object[] { 25, new(long, int)[] { (2, 22), (3, 10), (5, 6), (7, 3), (11, 2), (13, 1), (17, 1), (19, 1), (23, 1) } };
+        }
+    }
+}
---
+++ Tools/Factorization.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tools
+{
+    /// <summary>
+    /// Represents a factorization of a number
+    /// </summary>
+    public class Factorization : SortedDictionary<long, int>
+    {
+        #region Fields
+
+        /// <summary>
+        /// The empty factorization (ie. factorization of 1)
+        /// </summary>
+        private static Factorization _empty = new Factorization( Enumerable.Empty<(long, int)>() );
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructs the factorization from a sequence of primes and their exponents
+        /// </summary>
+        /// <param name="factors">The factors represented as a tuples of (prime, exponent)</param>
+        public Factorization( IEnumerable<(long Prime, int Exponent)> factors )
+        {
+            foreach( var item in factors )
+            {
+                Add( item.Prime, item.Exponent );
+            }
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override int GetHashCode()
+        {
+            return Count;
+        }
+
+        public override bool Equals( object obj )
+        {
+            var other = obj as Factorization;
+            if( other == null )
+                return false;
+
+            if( !this.OrderBy( i => i.Key ).SequenceEqual( other.OrderBy( i => i.Key ) ) )
+                return false;
+
+            return true;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns the factorization of a number
+        /// </summary>
+        /// <param name="number">The number to get the factorization of</param>
+        /// <returns>The factorization of <paramref name="number"/></returns>
+        public static Factorization Of( long number )
+        {
+            if( number < 1 )
+                throw new ArgumentOutOfRangeException( nameof( number ) );
+
+            if( number == 1 )
+                return Empty;
+
+            return new Factorization( Primes.Factor( number ) );
+        }
+
+        /// <summary>
+        /// Returns the factorization of a factorial
+        /// </summary>
+        /// <param name="number">The number for which the factorization of a factorial will be calculated</param>
+        /// <returns>The factorization of <paramref name="number"/>!</returns>
+        public static Factorization OfFactorial( int number )
+        {
+            return Enumerable.Range( 1, number ).Select( n => Of( n ) ).Aggregate( Empty, ( prev, cur ) => prev * cur );
+        }
+
+        /// <summary>
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the empty factorization (ie. the factorization of 1)
+        /// </summary>
+        public static Factorization Empty => _empty;
+
+        /// <summary>
+        /// Gets the collection of factors as a named tuple
+        /// </summary>
+        public IEnumerable<(long Prime, int Exponent)> Factors => this.Select( i => (i.Key, i.Value) );
+
+        #endregion
+
+        #region Operators
+
+        public static Factorization operator *( Factorization factors1, Factorization factors2 )
+        {
+            return new Factorization( Enumerable.Concat( factors1, factors2 ).GroupBy( p => p.Key ).Select( g => (g.Key, g.Sum( p => p.Value )) ) );
+        }
+
+        #endregion
+    }
+}
