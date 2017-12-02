b651b1ecba9945d26e28d4a947cb87fc010ea700
crypto_rsa <pkotrc@gmail.com>
2017-12-02 14:58:32 +0100

    Add class Numbers

---
+++ Tools.Tests/NumbersTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Tools.Tests
+{
+    [TestFixture]
+    internal class NumbersTests
+    {
+        [TestCase( 843, 0, 1 )]
+        [TestCase( 2, 2, 4 )]
+        [TestCase( 3, 5, 243 )]
+        [TestCase( 1500, 1, 1500 )]
+        [TestCase( 14, 7, 105_413_504 )]
+        public void Pow_ShouldReturnCorrectValue( long @base, long exponent, long expected )
+        {
+            Assert.That( Numbers.Pow( @base, exponent ), Is.EqualTo( expected ) );
+        }
+    }
+}
---
+++ Tools/Numbers.cs
@@ -0,0 +1,38 @@
+﻿namespace Tools
+{
+    /// <summary>
+    /// Contains methods for number manipulation
+    /// </summary>
+    public static class Numbers
+    {
+        #region Methods
+
+        /// <summary>
+        /// Performs fast exponentiation
+        /// </summary>
+        /// <param name="base">The base</param>
+        /// <param name="exponent">The exponent</param>
+        /// <returns><paramref name="base"/>^<paramref name="exponent"/></returns>
+        public static long Pow( long @base, long exponent )
+        {
+            long result = 1;
+            long temp = @base;
+            long tempExponent = exponent;
+
+            while( tempExponent > 0 )
+            {
+                if( (tempExponent & 1) == 1 )
+                {
+                    result *= temp;
+                }
+
+                temp *= temp;
+                tempExponent >>= 1;
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
