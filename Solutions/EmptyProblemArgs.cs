b4ad0881c1c55194af06fcb72ed58ef8cea05134
crypto_rsa <pkotrc@gmail.com>
2019-09-27 22:43:53 +0200

    Add class EmptyProblemArgs

---
+++ Solutions/EmptyProblemArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace EulerProblems.Solutions
+{
+    /// <summary>
+    /// Represents arguments for non-parametric problems
+    /// </summary>
+    public class EmptyProblemArgs
+    {
+        public static EmptyProblemArgs Instance { get; } = new EmptyProblemArgs();
+    }
+}
